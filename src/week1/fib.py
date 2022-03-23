fc140f1837a7457e5891ea447fdf23fa098dee36
gigiguan <89219495+gigiguan@users.noreply.github.com>
2022-03-23 14:22:33 -0700

    Update fib.py

--- src/week1/fib.py
+++ src/week1/fib.py
@@ -24,22 +24,36 @@ def tester():
 def fibonacci():
     count = int(input("Enter fibonacci sequence count: "))
     n = 0
-    while n <= count:
-      print(fibonacci1(n),end = " ")
-      n+=1
-    print()
-    return 
-  
-def fibonacci1(n):
+    while True:
+        if n <= count:
+            try:
+                print(fibonacci1(n),end = " ")
+                n+=1
+                print()
 
+            except ValueError:
+                print('Invalid input. Please enter an integer input.')
+        else:
+            return
+
+def fibonacci1(n):
+    # Check if input is 0 then it will
+    # print incorrect input
     if n < 0:
         print("Incorrect input")
+
+    # Check if n is 0
+    # then it will return 0
     elif n == 0:
         return 0
- 
+
+    # Check if n is 1,2
+    # it will return 1
     elif n == 1 or n == 2:
         return 1
- 
+
     else:
         return fibonacci1(n-1) + fibonacci1(n-2)
 
+if __name__ == "__main__":
+    fibonacci()
