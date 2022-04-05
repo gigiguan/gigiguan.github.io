61741fd8e544ae5149cad06f1eb5e74265b579f4
VidhiKulkarni <32562016+VidhiKulkarni@users.noreply.github.com>
2022-04-04 19:05:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,45 @@ import src.week0.tree
 import src.week1.hacks
 import src.week1.fib
 import src.week2.fac
+import src.week2.color
+import src.week0.ship
+import src.week2.factorialinput
+import src.week2.easteregg
 from src.week2.prime import Prime
 
+#Vidhi's Additions:
+# Colored Numbers
+# Ship with Color
+# Factorial With input
+# EasterEgg Submenu
+# Number Table
+# Word Expansion
+# Number Pattern
 
 main_menu = [
     ["Menu/Swap", src.week0.menu.print_menu2],
     ["Tree", src.week0.tree.tree],
     ["Fibonacci", src.week1.fib.fibonacci],
+    ["Color", src.week2.color.numb],
+    ["Ship", src.week0.ship.driver],
 ]
 
 sub_menu = [
     ["Primes", src.week2.prime.primes],
     ["Factorial", src.week2.fac.factorial],
+    ["Factorial Input", "src/week2/factorialinput.py"]
 ]
 
 patterns_sub_menu = [
     ["For Loop", src.week1.hacks.for_loop],
     ["While Loop", src.week1.hacks.while_loop],
     ["Recursive Loop", src.week1.hacks.recursive_loop],
+]
+
+easteregg_sub_menu = [
+    ["Word Expansion", src.week2.easteregg.word],
+    ["Number Table", src.week2.easteregg.rows],
+    ["Number Pattern", src.week2.easteregg.pattern]
 ]
 
 border = "=" * 25
@@ -39,14 +60,21 @@ def menu():
     menu_list = main_menu.copy()
     menu_list.append(["Loops", patterns_submenu])
     menu_list.append(["Math", submenu])
+    menu_list.append(["EasterEgg", easteregg_submenu])
     buildMenu(title, menu_list)
 
 def submenu():
     title = "Function Submenu" + banner
     buildMenu(title, sub_menu)
+  
+def easteregg_submenu():
+    title = "EasterEgg Submenu" + banner
+    buildMenu(title, easteregg_sub_menu)
+  
 def patterns_submenu():
-    title = "Patterns Submenu" + banner
-    buildMenu(title, patterns_sub_menu)
+  title = "Patterns Submenu" + banner
+  buildMenu(title, patterns_sub_menu)
+  
 
 def buildMenu(banner, options):
     # header for menu
